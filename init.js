d6f4110fd465e962485e9ac5131eb3f642d15e63
Shanglin Mo <shanglin_mo@outlook.com>
2021-01-30 17:49:12 -0500

    Update

---
+++ init.js
@@ -0,0 +1,24 @@
+<script src="assets/js/jquery.min.js"></script>
+
+var recipeList
+
+function loadData(){
+    var json = (function () {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "data/itemInfo.json",
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })(); 
+    recipeList = json.recipes
+}
+
+function getItem(itemName){
+    console.log(recipeList.itemName)
+}
